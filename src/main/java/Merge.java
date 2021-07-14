2b52d955bc528e9d421b0c7c58fff0820fb13166
belenbonilla <profbelenbonilla@gmail.com>
2021-07-14 18:29:09 -0500

    Se agrega clase Merge

---
+++ src/main/java/Merge.java
@@ -0,0 +1,27 @@
+public class Merge{
+
+    public static void  merge(int[ ] matrix, int init, int n1, int n2){
+        int[] buffer = new int[n1+n2];
+        int temp = 0;
+        int temp1 = 0;
+        int temp2 = 0;
+        int i;
+        while ((temp1 < n1) && (temp2 < n2)){
+            if (matrix[init + temp1] < matrix[init + n1 + temp2]){
+                buffer[temp++] = matrix[init + (temp1++)];
+            }else{
+                buffer[temp++] = matrix[init + n1 + (temp2++)];
+            }
+        }
+        while (temp1 < n1){
+            buffer[temp++] = matrix[init + (temp1++)];
+        }
+        while (temp2 < n2){
+            buffer[temp++] = matrix[init + n1 + (temp2++)];
+        }
+        for (i = 0; i < n1+n2; i++){
+            matrix[init + i] = buffer[i];
+        }
+    }
+    
+}
--- src/main/java/Ordenamiento.java
+++ src/main/java/Ordenamiento.java
@@ -1,14 +1,14 @@
 public class Ordenamiento{
-    public static void burbuja(int[]matrix){
-        int temp;
-        for(int i=1;i < matrix.length;i++){
-            for (int j=0 ; j < matrix.length- 1; j++){
-                if (matrix[j] > matrix[j+1]){
-                    temp = matrix[j];
-                    matrix[j] = matrix[j+1];
-                    matrix[j+1] = temp;
-                }
+ public static void burbuja(int[]matrix){
+    int temp;
+    for(int i=1;i < matrix.length;i++){
+        for (int j=0 ; j < matrix.length- 1; j++){
+            if (matrix[j] > matrix[j+1]){
+                temp = matrix[j];
+                matrix[j] = matrix[j+1];
+                matrix[j+1] = temp;
             }
         }
     }
+}
 }
